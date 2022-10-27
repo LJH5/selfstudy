6107229ac2e87e34423a2adafac5bd5091bf290f
LJH5 <sigma6317@gmail.com>
2022-10-27 23:43:15 +0900

    '2022-10-27-commit'

--- Algorithm/Algorithm_Problem/백준/Silver/10157.py
+++ Algorithm/Algorithm_Problem/백준/Silver/10157.py
@@ -1,19 +1,42 @@
-def spiral():
+def spiral(r, c, cnt):
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
-    r, c = 1, 1
-    for k in range(4):
-        for _ in range(max(R, C)):
-            if not(1 <= r < R and 1 <= c < C):
-                r -= dr[k]
-                c -= dc[k]
-                break
-            print(r, c)
-            r += dr[k]
-            c += dc[k]
+
+    visited.add((r, c))
+    while cnt < K:
+        for k in range(4):
+            for _ in range(max(R, C)):
+                r += dr[k]
+                c += dc[k]
+                if r < 1 or r > C or c < 1 or c > R or (r, c) in visited:
+                    r -= dr[k]
+                    c -= dc[k]
+                    break
+                visited.add((r, c))
+                cnt += 1
+                if check(cnt):
+                    return r, c
+
+
+def check(count):
+    if count == K:
+        return True
+    else:
+        return False
 
 
 C, R = map(int, input().split())
 K = int(input())
 
-spiral()
+visited = set()
+start_r, start_c = 1, 1
+start_cnt = 1
+if K > C*R:
+    print(0)
+else:
+    if check(start_cnt):
+        result = (start_r, start_c)
+    else:
+        result = spiral(start_r, start_c, start_cnt)
+
+    print(*result)

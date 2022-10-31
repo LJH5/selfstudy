d56428b4cb2e4ec874b0f64c9f8ae2ef6a28d881
LJH5 <sigma6317@gmail.com>
2022-10-31 23:17:00 +0900

    '2022-10-31-commit'

---
+++ Django/2210/221030/static/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contri
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = Aut
--- Vue/221031/test/05_constructor_func.js
+++ Vue/221031/test/05_constructor_func.js
@@ -3,17 +3,21 @@ const member = {
   age: 22,
   sId: 2022311491,
 }
+console.log(member)
 
-// const member2 = {
-//   name: 'haley',
-//   age: 20,
-//   sId: 2022311492,
-// }
+const member2 = {
+  name: 'haley',
+  age: 20,
+  sId: 2022311492,
+}
+console.log(member2)
 
-// function Member(name, age, sId) {
-//   this.name = name
-//   this.age = age
-//   this.sId = sId
-// }
+// 생성자 함수
+function Member(name, age, sId) {
+  this.name = name
+  this.age = age
+  this.sId = sId
+}
 
-// const member3 = new Member('isaac', 21, 2022654321)
+const member3 = new Member('issac', 21, 2022654321)
+console.log(member3)

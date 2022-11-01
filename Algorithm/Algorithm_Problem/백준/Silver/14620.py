5f4ee5f9f1094554b1c1e4a81e1b9c96c6f8deb1
ljh <sigma6317@gmail.com>
2022-11-02 08:54:29 +0900

    '2022-11-02-commit'

--- Algorithm/Algorithm_Problem/백준/Silver/14620.py
+++ Algorithm/Algorithm_Problem/백준/Silver/14620.py
@@ -2,7 +2,6 @@
 # +로 피는 꽃을 겹치는 부분이 없이 심을 때
 # 최소비용을 구하여라
 # 씨앗은 총 3개
+import sys
+field = [list(map(int, sys.stdin.readline().split())) for _ in range(int(input()))]  # 이게 되는구나... 뒤에 input이 먼저 실행되는 구나
 
-field = [list(map(int, input().split())) for _ in range(int(input()))]  # 이게 되는구나... 뒤에 input이 먼저 실행되는 구나
-
-print(field)

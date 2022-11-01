87ade57bf8f9afdc52691d5482d98413f89b0d7c
ljh <sigma6317@gmail.com>
2022-11-01 17:58:09 +0900

    '2022-11-01-commit'

--- Algorithm/Algorithm_Problem/백준/Silver/11048.py
+++ Algorithm/Algorithm_Problem/백준/Silver/11048.py
@@ -1,32 +1,25 @@
 # 이동하기
-import heapq
+def find(r, c, tmp_sum):
+    global result
+    if (r, c) == (N-1, M-1):    # 목적지에 도착
+        if tmp_sum > result:    # 사탕의 합이 최대값보다 많으면
+            result = tmp_sum    # 최대값 갱신
+        return
 
-def dijstra():
-    dist = [[0] * M for _ in range(N)]
-    visited = set()
-    heap = []
-    heapq.heappush(heap, (0, 0))
-    dr = [1, 0, 1]
-    dc = [0, 1, 1]
-    while heap:
-        r, c = heapq.heappop(heap)
-
-        if (r, c) not in visited:
-            dist[r][c] = maze[r][c]
-            visited.add((r, c))
-
-            for k in range(3):
-                r += dr[k]
-                c += dc[k]
-                if (r, c) in visited:
-                    continue
-                if not(0 <= r < N and 0 <= c < M):
-                    continue
-
-                heapq.heappush(heap, (r, c))
+    for k in range(3):          # 3방으로 탐색하여
+        nr = r + dr[k]
+        nc = c + dc[k]
+        if 0 <= nr < N and 0 <= nc < M:   # 이동 좌표가 미로 안에 있으면
+            if maze[nr][nc] < tmp_sum + maze[r][c]:
+                maze[nr][nc] = tmp_sum + maze[r][c]
 
 
 N, M = map(int, input().split())
 maze = [list(map(int, input().split())) for _ in range(N)]
 result = 0
-dijstra()
+dr = [0, 1, 1]
+dc = [1, 1, 0]
+
+find(0, 0, maze[0][0])
+
+print(result)

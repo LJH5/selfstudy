685d1075478470c2fe6e453cc43fa8cb51257c36
ljh <sigma6317@gmail.com>
2022-11-03 17:52:44 +0900

    '2022-11-03-commit'

---
+++ Java/221102/src/Java04/control/Loop02.java
@@ -0,0 +1,15 @@
+package Java04.control;
+
+public class Loop02 {
+
+	public static void main(String[] args) {
+		int n = 0;
+		while (n <= 5) {
+			System.out.println("이것은 실행이 될까?");
+			n++;
+		}
+			
+
+	}
+
+}

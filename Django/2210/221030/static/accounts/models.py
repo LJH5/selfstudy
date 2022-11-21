07f7116a0ad333831fb889eb175529d8abc93196
ljh <sigma6317@gmail.com>
2022-11-21 21:12:18 +0900

    221121

--- Django/2210/221030/static/accounts/models.py
+++ Django/2210/221030/static/accounts/models.py
@@ -1,6 +1,21 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
 class User(AbstractUser):
-    pass
+    # 추가한 필드 : profile_image, point
+    profile_image = ProcessedImageField(
+        blank=True,
+        upload_to='profile_image/%Y/%m',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 70},
+    )
+
+    point = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+    )

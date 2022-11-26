413a3bd316ad2aa0b2c953607df623b7a820f1a6
ljh <sigma6317@gmail.com>
2022-11-26 23:42:56 +0900

    gomgom

--- Algorithm/Algorithm_Problem/BOJ/Silver/18111.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/18111.py
@@ -1,7 +1,9 @@
 # 마인크래프트
 # N: 세로, M: 가로, B: 인벤토리에 블럭 개수
+import sys
+input = sys.stdin.readline
+
 N, M, B = map(int, input().split())
-matrix = list(map(int, input().split()) for _ in range(N))
+matrix = list(map(int, input()) for _ in range(N))
 
-
-
+print(matrix)

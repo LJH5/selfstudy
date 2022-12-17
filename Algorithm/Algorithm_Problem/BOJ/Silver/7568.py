c856782fb2d50e8ae56f272faafa8e8b10515fef
ljh <sigma6317@gmail.com>
2022-12-17 21:52:14 +0900

    221217

--- Algorithm/Algorithm_Problem/BOJ/Silver/7568.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/7568.py
@@ -1,1 +1,16 @@
 # 덩치
+n = int(input())
+size = [list(map(int, input().split())) for _ in range(n)]
+grade = [0]*n
+rank = 1
+
+for i in range(n):
+    tmp_x = size[i][0]
+    tmp_y = size[i][1]
+    rank = 1
+    for j in range(n):
+        if tmp_x < size[j][0] and tmp_y < size[j][1]:
+            rank += 1
+    grade[i] = rank
+
+print(*grade)

8bb775c970a161548cccda0f64d14c3423ea380d
ljh <sigma6317@gmail.com>
2023-01-05 10:07:19 +0900

    '2023-01-05-commit'

---
+++ React/JS/Funtion/function05.js
@@ -0,0 +1,10 @@
+// 함수형
+const log = function(message) {
+  console.log(message)
+}
+log('자바스크립트는 함수를 변수에 넣을 수 있음')
+
+const log2 = message => {
+  console.log(message)
+}
+log2('자바스크립트는 함수를 변수에 넣을 수 있음')

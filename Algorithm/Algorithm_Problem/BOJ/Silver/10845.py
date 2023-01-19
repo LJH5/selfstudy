db3c9c13898d805874acb2d47b4726fe9df2d15f
ljh <sigma6317@gmail.com>
2023-01-20 00:05:02 +0900

    '2023-01-20-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/10845.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/10845.py
@@ -1,24 +1,26 @@
 import sys
 input = sys.stdin.readline
-# 해당 방식으로 입력받으면 '\n'이 들어가는 것을 주의하자!!!
-n = int(input())
-stack = []
+from collections import deque
 
+n = int(input())
+q = deque()
 for _ in range(n):
     commend = list(input().split())
     try:
         if commend[0] == 'push':
-            stack.append(commend[1])
+            q.append(commend[1])
         elif commend[0] == 'pop':
-            print(stack.pop())
+            print(q.popleft())
         elif commend[0] == 'size':
-            print(len(stack))
+            print(len(q))
         elif commend[0] == 'empty':
-            if stack:
-               print(0)
+            if q:
+                print(0)
             else:
                 print(1)
+        elif commend[0] == 'front':
+            print(q[0])
         else:
-            print(stack[-1])
+            print(q[-1])
     except:
         print('-1')

6fd653b5e7ad8763d892135b9f3dd7e29af54aab
ljh <sigma6317@gmail.com>
2023-01-25 22:11:51 +0900

    '2023-01-25-commit'

---
+++ React/crud/test/src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import { useState } from 'react';
+
+function Header(props) {
+  return <header>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault()
+      props.sampleEvent()
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props) {
+  // console.log(props)
+  const lis = []
+  for (let i=0; i<props.topics.length; i++) {
+    const t = props.topics[i]
+    lis.push(<li key={t.id}><a id={t.id} href={"/read/"+t.id} onClick={(event) => {
+      event.preventDefault()
+      // console.log(event.target)
+      props.sampleEvent(t.id)
+      // console.log(typeof(event.target.id))  // string
+    }}>{t.title}</a></li>)
+  }
+  // console.log(lis)
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+function Article(props) {
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
+function App() {
+  const [mode, setMode] = useState("WELCOME")
+  const [id, setId] = useState(null)
+  let content = null
+  const topics = [
+    {id: 1, title: 'html', body: 'html is ...'},
+    {id: 2, title: 'css', body: 'css is ...'},
+    {id: 3, title: 'js', body: 'js is ...'},
+  ]
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB" />
+  } else if (mode === "READ") {
+    let title, body = null
+    for (let i=0; i<topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title
+        body = topics[i].body
+      }
+    }
+    content = <Article title={title} body={body} />
+  }
+  return (
+    <div className="App">
+      <Header title="WEB" sampleEvent={() => {setMode("WELCOME")}}/>
+      <Nav topics={topics} sampleEvent={(myId) => {
+        setMode("READ")
+        setId(myId)
+      }}/>
+      {content}
+    </div>
+  );
+}
+
+export default App;

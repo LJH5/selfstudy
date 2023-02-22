05f243bb240d042dc8c865ef16957c05c50f8b2c
ljh <sigma6317@gmail.com>
2023-02-22 13:38:13 +0900

    '2023-02-22-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/11659.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/11659.py
@@ -4,6 +4,11 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 
-for i in range(m):
+dp = [0] * (n + 1)
+
+for i in range(n):
+    dp[i + 1] = dp[i] + numbers[i]
+
+for _ in range(m):
     s, e = map(int, input().split())
-    print(sum(numbers[s-1:e]))
+    print(dp[e] - dp[s - 1])

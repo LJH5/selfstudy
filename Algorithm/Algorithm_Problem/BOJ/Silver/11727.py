2357a8f4145f7c87b38a7bfea086fbb48cdb5956
ljh <sigma6317@gmail.com>
2023-02-26 00:29:25 +0900

    '2023-02-26-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/11727.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/11727.py
@@ -2,8 +2,7 @@ n = int(input())
 dp = [0] * 1001
 dp[1] = 1
 dp[2] = 3
-dp[3] =
-for i in range(4, 1001):
-    dp[i] = dp[i - 2] + dp[i - 1]
+for i in range(3, n + 1):
+    dp[i] = dp[i - 2] * 2 + dp[i - 1]
 
-print(dp[n] % 1007)
+print(dp[n] % 10007)

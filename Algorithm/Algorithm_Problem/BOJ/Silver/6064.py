3e69e48ee3f8e7302329d804e5bc4554b1cd153a
ljh <sigma6317@gmail.com>
2023-03-24 22:55:23 +0900

    '2023-03-24-commit'

---
+++ Algorithm/Algorithm_Problem/BOJ/Silver/6064.py
@@ -0,0 +1,12 @@
+# 1 ≤ m, n ≤ 40,000, 1 ≤ x ≤ m, 1 ≤ y ≤ n
+# x' = x + 1 m < x이면 x' = 1
+# <x':y'>
+# <n, m>은 마지막
+def check():
+    pass
+
+
+t = int(input())
+for _ in range(t):
+    m, n, x, y = map(int, input().split())
+

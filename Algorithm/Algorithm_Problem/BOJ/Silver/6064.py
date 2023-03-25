c598f6be153d9af63a93544c23e3174cf7913652
ljh <sigma6317@gmail.com>
2023-03-25 10:51:43 +0900

    '2023-03-25-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/6064.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/6064.py
@@ -3,10 +3,16 @@
 # <x':y'>
 # <n, m>은 마지막
 def check():
-    pass
+    k = x
+    while k <= m * n:
+        if (k - x) % m == 0 and (k - y) % n == 0:
+            return k
+        k += m
+    return -1
 
 
 t = int(input())
 for _ in range(t):
     m, n, x, y = map(int, input().split())
 
+    print(check())

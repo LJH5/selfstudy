b3912e080a1dbe38961e5a4d67fe02bf903b0699
ljh <sigma6317@gmail.com>
2023-03-29 22:23:07 +0900

    '2023-03-29-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/1931.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/1931.py
@@ -3,5 +3,8 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 n = int(input())
 time_list = [tuple(map(int, input().split())) for _ in range(n)]
+time_list = sorted(time_list, key=lambda x: (x[0], x[1]))    # 정렬하기
 
-print(sorted(time_list, key = lambda x : (x[0], x[1])))
+for i in range(n):
+    start_time, end_time = time_list[i]
+    for j in range(n - i):

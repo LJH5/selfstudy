06af2ecc1bd784f3f05ee34ec1a466f5deead61e
ljh <sigma6317@gmail.com>
2023-03-30 22:58:51 +0900

    '2023-03-30-commit'

--- Algorithm/Algorithm_Problem/BOJ/Silver/1931.py
+++ Algorithm/Algorithm_Problem/BOJ/Silver/1931.py
@@ -1,10 +1,9 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip('\r\n')
-
-n = int(input())
-time_list = [tuple(map(int, input().split())) for _ in range(n)]
-time_list = sorted(time_list, key=lambda x: (x[0], x[1]))    # 정렬하기
-
-for i in range(n):
-    start_time, end_time = time_list[i]
-    for j in range(n - i):
+# import sys
+# input = lambda: sys.stdin.readline().rstrip('\r\n')
+#
+# n = int(input())
+# time_list = [tuple(map(int, input().split())) for _ in range(n)]
+# time_list = sorted(time_list, key=lambda x: (x[0], x[1]))    # 정렬하기
+# # 시작 시간이 같을때 짧은 거만 남기고 긴거는 삭제해도 될듯?
+a = [(1, 2), (1, 1)]
+print(set(a))
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# 1주 5백준  class 3 
+# 1주 5백준  class 3
 
-# ROS2
-
-- path tracking
-- wall tracking
+# 

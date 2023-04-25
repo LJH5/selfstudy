8bc59c4d4432934854f32482dd591e096a7883a0
ljh <sigma6317@gmail.com>
2023-04-26 06:14:57 +0900

    '2023-04-26-commit'

---
+++ C++/main/main/Source.cpp
@@ -0,0 +1,17 @@
+/*
+* This is my code
+*/
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int x = 2;		// 정수형 데이터 변수 메모리 할당
+	x = 5;
+	int  y = x + 3;
+	
+	std::cout << y << std::endl;
+	std::cout << 1*3 << std::endl;
+
+	return 0;
+}

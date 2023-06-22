275fcff457a72b56b6efbb390e9835aadb98d304
LJH5 <sigma6317@gmail.com>
2023-06-22 19:01:54 +0900

    '2023-06-22-commit'

--- Algorithm/C++/Exe/Source.cpp
+++ Algorithm/C++/Exe/Source.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			cout << "*";
-		}
-		cout << "\n";
-	}
+int main(void) {
+	vector<int> v(100);
+	v[101] = 100;
+	
+	cout << v[101];
 
 	return 0;
 }

0cd24f0036e2ac37bc943ee0a7fa1247b89d91c5
LJH5 <sigma6317@gmail.com>
2023-07-08 12:40:45 +0900

    '2023-07-08-commit'

--- C++/exe/Source1.cpp
+++ C++/exe/Source1.cpp
@@ -5,23 +5,17 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cout << "1차원 배열 \n";
-	int arr[5] = { 1, 2, 3, 4, 5 };
-	for (auto i : arr) {
-		cout << i << " ";
-	}
-	cout << "\n\n";
-
-	cout << "2차원 배열 \n";
-	int matrix[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
-	for (const auto& row : matrix) {
-		for (const auto& col : row) {
+	vector<int> v1(3, 5);			// {5, 5, 5};
+	cout << v1.size() << "\n";	// 3
+	v1.push_back(7);				// {5, 5, 5, 7};
+	
+	vector<vector<int>> v2 = { {1, 2, 3}, {4, 5, 6} };
+	for (const auto row : v2) {
+		for (const auto col : row) {
 			cout << col << " ";
 		}
 		cout << "\n";
 	}
-
-	// 방법1 memset
 
 	return 0;
 }

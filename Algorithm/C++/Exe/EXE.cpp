6f1a60d7fdb501549828efff5afb828caab33882
LJH5 <sigma6317@gmail.com>
2023-07-17 23:52:20 +0900

    '2023-07-17-commit'

--- Algorithm/C++/Exe/EXE.cpp
+++ Algorithm/C++/Exe/EXE.cpp
@@ -5,14 +5,20 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, x;
-	cin >> n >> x;
+	int n, m, target_r, target_c;
+	cin >> n >> m;
+	
+	char people_arr[601][601] = {};
+
 	for (int i = 0; i < n; i++) {
-		int num;
-		cin >> num;
-		if (num < x)
-			cout << num << " ";
+		for (int j = 0; j < m; j++) {
+			cin >> people_arr[i][j];
+			if (people_arr[i][j] == 'I') {
+				target_r = i, target_c = j;
+			}
+		}
 	}
+
 
 	return 0;
 }
